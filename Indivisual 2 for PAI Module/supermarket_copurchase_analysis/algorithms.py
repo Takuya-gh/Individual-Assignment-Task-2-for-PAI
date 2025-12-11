1f53333d1ffa6495e027847f9b912fc5052d03ac
Takuya Otsuki <tsukky333@gmail.com>
2025-12-11 15:34:57 +0000

    feat: implement build_graph_from_transactions

--- Indivisual 2 for PAI Module/supermarket_copurchase_analysis/algorithms.py
+++ Indivisual 2 for PAI Module/supermarket_copurchase_analysis/algorithms.py
@@ -1,1 +1,21 @@
 # Algorithms for graph building and queries
+from supermarket_copurchase_analysis.graph_model import CoPurchaseGraph
+
+
+def build_graph_from_transactions(transactions):
+    """
+    Create a CoPurchaseGraph and populate it from the list of transactions.
+
+    Args:
+        transactions (list): List of transactions, where each transaction
+                           is a list of item names.
+
+    Returns:
+        CoPurchaseGraph: The populated graph.
+    """
+    graph = CoPurchaseGraph()
+    
+    for transaction in transactions:
+        graph.add_transaction(transaction)
+    
+    return graph
